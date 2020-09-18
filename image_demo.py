2d4a49eef3a469082e718358c6dd5f34f668204e
hgc <hgc@18join.com>
2020-09-18 17:33:14 +0800

    add:pd 06

--- image_demo.py
+++ image_demo.py
@@ -18,14 +18,16 @@ import tensorflow as tf
 from PIL import Image
 
 return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"]
-pb_file         = "./yolov3_coco.pb"
-image_path      = "./docs/images/road.jpeg"
-num_classes     = 80
+
+pb_file         = "E:/dm/repo/tensorflow-yolov3/yolov3_slider_04.pb"
+image_path      = "E:/dm/repo/tensorflow-yolov3/docs/images/jy5.jpg"
+num_classes     = 1
 input_size      = 416
+
 graph           = tf.Graph()
 
 original_image = cv2.imread(image_path)
-original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
 original_image_size = original_image.shape[:2]
 image_data = utils.image_preporcess(np.copy(original_image), [input_size, input_size])
 image_data = image_data[np.newaxis, ...]
@@ -44,6 +46,7 @@ pred_bbox = np.concatenate([np.reshape(pred_sbbox, (-1, 5 + num_classes)),
 
 bboxes = utils.postprocess_boxes(pred_bbox, original_image_size, input_size, 0.3)
 bboxes = utils.nms(bboxes, 0.45, method='nms')
+original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 image = utils.draw_bbox(original_image, bboxes)
 image = Image.fromarray(image)
 image.show()
