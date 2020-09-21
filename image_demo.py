00f434e1a59b2806bf83a764d9c06616e39149ee
hgc <hgc@18join.com>
2020-09-21 09:42:40 +0800

    add:init weight#0.2437.ckpt-5

--- image_demo.py
+++ image_demo.py
@@ -19,7 +19,7 @@ from PIL import Image
 
 return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"]
 
-pb_file         = "./yolov3_slider_06.pb"
+pb_file         = "./yolov3_slider_07.pb"
 image_path      = "E:/dm/repo/tensorflow-yolov3/docs/images/sn.jpg"
 num_classes     = 1
 input_size      = 416
